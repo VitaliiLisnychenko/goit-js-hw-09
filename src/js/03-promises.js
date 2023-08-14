30059353aec7c16bf749af1f744b7f9a261b79df
VitaliiLisnychenko <lisnychenkovitalii@gmail.com>
2023-08-14 15:58:57 +0200

    third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('form');
+formEl.addEventListener('submit', onSubmitClick);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmitClick(evt) {
+  evt.preventDefault();
+
+  let delay = Number(evt.target.elements.delay.value);
+  let step = Number(evt.target.elements.step.value);
+  let amount = Number(evt.target.elements.amount.value);
+
+  for (let i = 1; i < amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
   }
 }
